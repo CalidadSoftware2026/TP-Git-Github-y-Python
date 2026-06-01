b49496478316982aa4e67c75426ccee831263139
Valen <vfunesa[200~gmail.com>
2026-06-01 15:58:20 -0300

    apliqué ruff formatting (y creé mi entorno virtual)

--- tests/test_person.py
+++ tests/test_person.py
@@ -2,11 +2,7 @@ from src.person import Person
 
 
 def test_create_person():
-    person = Person(
-        "Octavio",
-        24,
-        "octavio@email.com"
-    )
+    person = Person("Octavio", 24, "octavio@email.com")
 
     assert person.name == "Octavio"
     assert person.age == 24
@@ -14,20 +10,12 @@ def test_create_person():
 
 
 def test_is_adult_true():
-    person = Person(
-        "Juan",
-        20,
-        "juan@email.com"
-    )
+    person = Person("Juan", 20, "juan@email.com")
 
     assert person.is_adult() is True
 
 
 def test_is_adult_false():
-    person = Person(
-        "Pedro",
-        15,
-        "pedro@email.com"
-    )
+    person = Person("Pedro", 15, "pedro@email.com")
 
     assert person.is_adult() is False
